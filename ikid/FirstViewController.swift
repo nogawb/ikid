c80a3d00460ec04a07214c195321ed31959fe96b
Benjamin Nogawa <benjamin.nogawa@gmail.com>
2018-02-06 22:12:01 -0800

    initial commit

---
+++ ikid/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ikid
+//
+//  Created by Ben Nogawa on 2/6/18.
+//  Copyright © 2018 Ben Nogawa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
